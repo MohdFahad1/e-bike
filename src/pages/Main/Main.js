2eaf13689b5f7ec7ed63844406b7079cf6302e41
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2023-05-25 10:31:28 +0530

    Route added

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Home from "../Home/Home"
+import Product from "../Product/Product"
+import About from "../About/About"
+import Contact from "../Contact/Contact"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <>
+        <BrowserRouter>
+            <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="product" element={<Product />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+            </Routes>
+        </BrowserRouter>
+    </>
   )
 }
 
