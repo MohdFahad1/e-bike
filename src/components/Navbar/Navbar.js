b6d85a33e47f2f413658de61653d1e133fa1554a
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2023-05-27 13:16:27 +0530

    Navbar Fixed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,33 +3,20 @@ import "./Navbar.css"
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-  const [isScrolled, setIsScrolled] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollTop = window.pageYOffset;
-      setIsScrolled(scrollTop > 0);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
 
   return (
     <>
-      <header className={`header ${isScrolled ? 'scrolled' : ''}`}>
-        <Link to="/" className="logo">Unicus</Link>
+      <header className="header">
+        <Link to="/" className="logo">Unicus Mobility</Link>
         <input className="menu-btn" type="checkbox" id="menu-btn" />
         <label className="menu-icon" htmlFor="menu-btn">
           <span className="navicon"></span>
         </label>
         <ul className="menu">
-          <li><Link to="/" className="link link-theme link-arrow">HOME</Link></li>
-          <li><Link to="/product" className="link link-theme link-arrow">PRODUCTS</Link></li>
-          <li><Link to="/about" className="link link-theme link-arrow">ABOUT US</Link></li>
-          <li><Link to="/contact" className="link link-theme link-arrow">CONTACT</Link></li>
+          <li><Link to="/" className="link link-theme link-arrow" style={{color:"#fff"}}>HOME</Link></li>
+          <li><Link to="/product" className="link link-theme link-arrow" style={{color:"#fff"}}>PRODUCTS</Link></li>
+          <li><Link to="/about" className="link link-theme link-arrow" style={{color:"#fff"}}>ABOUT US</Link></li>
+          <li><Link to="/contact" className="link link-theme link-arrow" style={{color:"#fff"}}>CONTACT</Link></li>
         </ul>
       </header>
     </>
