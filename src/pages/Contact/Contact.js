30dba28d62e7f7eab263fa9b4da93c898e10eefb
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2023-05-27 12:21:10 +0530

    responsiveness added to contact page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,13 +1,17 @@
-import React from 'react'
-import { FaInstagram, FaLinkedinIn, FaTwitter, FaPaperPlane, FaMapMarkedAlt, FaPhone, FaRegEnvelope } from "react-icons/fa";
+import React, { useState } from 'react';
+import { FaInstagram, FaLinkedinIn, FaTwitter, FaPaperPlane, FaMapMarkedAlt, FaPhone, FaRegEnvelope } from 'react-icons/fa';
 import './Contact.css';
 
 const ContactPage = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    e.target.elements.name.value = '';
-    e.target.elements.email.value = '';
-    e.target.elements.message.value = '';
+    setName('');
+    setEmail('');
+    setMessage('');
   };
 
   return (
@@ -16,24 +20,50 @@ const ContactPage = () => {
 
       <div className="contact-wrapper">
         {/* Left contact page */}
-        <form id="contact-form" className="form-horizontal" role="form" onSubmit={handleSubmit} autoComplete='off'>
+        <form id="contact-form" className="form-horizontal" role="form" onSubmit={handleSubmit} autoComplete="off">
           <div className="form-group">
             <div>
-              <input type="text" className="form-control" id="name" placeholder="NAME" name="name" value="" required />
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                placeholder="NAME"
+                name="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
             </div>
           </div>
 
           <div className="form-group">
             <div>
-              <input type="email" className="form-control" id="email" placeholder="EMAIL" name="email" value="" required />
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                placeholder="EMAIL"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
             </div>
           </div>
 
-          <textarea className="form-control" rows="10" placeholder="MESSAGE" name="message" required></textarea>
+          <textarea
+            className="form-control"
+            rows="10"
+            placeholder="MESSAGE"
+            name="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            required
+          ></textarea>
 
           <button className="send-button" type="submit" value="SEND">
             <div className="alt-send-button">
-              <FaPaperPlane className='send-btn-icon'/>
+              <FaPaperPlane className="send-btn-icon" />
             </div>
           </button>
         </form>
@@ -41,29 +71,38 @@ const ContactPage = () => {
         {/* Right contact page */}
         <div className="direct-contact-container">
           <ul className="contact-list">
-            <li className="list-item"><FaMapMarkedAlt className='contact-icon'/><span className="contact-text place">Location</span></li>
+            <li className="list-item">
+              <FaMapMarkedAlt className="contact-icon" />
+              <span className="contact-text place">Location</span>
+            </li>
 
-            <li className="list-item"><FaPhone className='contact-icon'/><span className="contact-text phone">Phone</span></li>
+            <li className="list-item">
+              <FaPhone className="contact-icon" />
+              <span className="contact-text phone">Phone</span>
+            </li>
 
-            <li className="list-item"><FaRegEnvelope className='contact-icon'/><span className="contact-text gmail">Email</span></li>
+            <li className="list-item">
+              <FaRegEnvelope className="contact-icon" />
+              <span className="contact-text gmail">Email</span>
+            </li>
           </ul>
 
-          <hr style={{marginTop:"20px"}}/>
+          <hr style={{ marginTop: '20px' }} />
 
           <ul className="social-media-list">
             <li>
-              <a href="#" >
-                <FaInstagram className='social-icon'/>
+              <a href="#">
+                <FaInstagram className="social-icon" />
               </a>
             </li>
             <li>
-              <a href="#" >
-                <FaLinkedinIn className='social-icon'/>
+              <a href="#">
+                <FaLinkedinIn className="social-icon" />
               </a>
             </li>
             <li>
-              <a href="#" >
-                <FaTwitter className='social-icon'/>
+              <a href="#">
+                <FaTwitter className="social-icon" />
               </a>
             </li>
           </ul>
@@ -71,6 +110,6 @@ const ContactPage = () => {
       </div>
     </section>
   );
-}
+};
 
 export default ContactPage;
